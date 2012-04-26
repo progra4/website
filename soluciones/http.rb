e88e7f6e43e18cfe09e6662d3bcd1ca66dc858fe
Luis Borjas <luisfborjas@gmail.com>
2012-04-26 03:00:15 -0600

    la solución usando bloques

--- soluciones/http.rb
+++ soluciones/http.rb
@@ -8,7 +8,15 @@ require 'json'
 #4. Obtener /read con el header de cookies
 #
 
+
 def get_response(uri, req)
+  Net::HTTP.start(uri.hostname, uri.port){|http|  http.request(req) }
+end
+
+def get(url, &builder)
+  uri = URI(url)
+  req = Net::HTTP::Get.new(uri.request_uri)
+  builder[req]
   Net::HTTP.start(uri.hostname, uri.port){|http|  http.request(req) }
 end
 
@@ -19,10 +27,9 @@ end
 host = "http://httparty.herokuapp.com"
 
 #1. Obtener las ids
-list_uri = URI("#{host}/quotes")
-list_req = Net::HTTP::Get.new(list_uri.request_uri)
-list_req['Accept'] = "application/json"
-list_res = get_response(list_uri, list_req)
+list_res = get("#{host}/quotes") do |req|
+  req['Accept'] = "application/json"
+end
 ids = parse_json(list_res).map{|quote| quote["id"] }
 
 #2. Elegir tres al azar
@@ -33,22 +40,23 @@ puts "I've chosen: #{sample.inspect}"
 cookie = ""
 
 sample.each do |quote_id|
-  quote_uri = URI("#{host}/quotes/#{quote_id}")
-  quote_req = Net::HTTP::Get.new(quote_uri.request_uri)
-  quote_req['Accept'] = "application/json"
-  quote_req['Cookie'] = cookie unless cookie.empty?
-  list_res = get_response(quote_uri, quote_req)
-  cookie = list_res['Set-Cookie']
+
+  quote_res = get("#{host}/quotes/#{quote_id}") do |req|
+    req['Accept'] = "application/json"
+    req['Cookie'] = cookie unless cookie.empty?
+  end
+
+  cookie = quote_res['Set-Cookie']
 end
 
 #Obtener las leídas
 #
 puts "Quotes I've read:"
 
-read_uri = URI("#{host}/quotes/read")
-read_req = Net::HTTP::Get.new(read_uri.request_uri)
-read_req['Accept'] = "text/plain"
-read_req['Cookie'] = cookie
-read_res = get_response(read_uri, read_req)
-puts read_res.body
+b = get("#{host}/quotes/read") do |req|
+  req['Accept'] = "text/plain"
+  req['Cookie'] = cookie
+end.body
+
+puts b
 
